a501b359b8c01abb9b322ddde16f0606d6ae7ecf
Dionysus <DorseyM5A97@gmail.com>
2013-06-05 11:15:40 -0400

    Removed Datablocks.cs
    transferred Datablocks to Common.cs

--- Gamemode_City3/Common.cs
+++ Gamemode_City3/Common.cs
@@ -4,3 +4,35 @@
 // Created on         :  Tuesday, June 4, 2013 2:02 PM
 // Description        :  Common operations of City3
 // ============================================================
+
+// ============================================================
+// Section 1: Datablocks
+// ============================================================
+
+datablock ItemData(droppedcashItem)
+{
+	category = "Weapon";
+	className = "Weapon";
+	
+	shapeFile = "base/data/shapes/brickWeapon.dts";
+	mass = 1;
+	density = 0.2;
+	elasticity = 0.2;
+	friction = 0.6;
+	emap = true;
+	
+	doColorShift = true;
+	colorShiftColor = "0 0.5 0 1";
+	image = dropcashImage;
+	candrop = true;
+	canPickup = false;
+};
+datablock ShapeBaseImageData(droppedcashImage)
+{
+	shapeFile = "base/data/shapes/brickWeapon.dts";
+	emap = true;
+	
+	doColorShift = true;
+	colorShiftColor = dropCashItem.colorshiftcolor;
+	canPickup = false;
+};
--- Gamemode_City3/Labor.cs
+++ Gamemode_City3/Labor.cs
@@ -1,13 +1,6 @@
 // ============================================================
 // Project            :  City3
-// File               :  .\Labor.cs
-// Copyright          :  
-// Author             :  Andrew
-// Created on         :  Tuesday, June 4, 2013 2:58 PM
-//
-// Editor             :  TorqueDev v. 1.2.3430.42233
-//
-// Description        :  
-//                    :  
-//                    :  
+// File               :  .\Common.cs
+// Created on         :  Tuesday, June 4, 2013 2:02 PM
+// Description        :  Labor-related operations
 // ============================================================
